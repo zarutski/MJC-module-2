3f1200f5bd543c83ab7f756cc1b346a6ce4e74aa
Maksim Zarutski <zarutski93maksim@gmail.com>
2021-03-30 07:38:39 +0300

    module-6 initial

--- web/src/main/java/com/epam/esm/web/util/SecurityValue.java
+++ web/src/main/java/com/epam/esm/web/util/SecurityValue.java
@@ -15,7 +15,7 @@ public final class SecurityValue {
     public static final String KEYCLOAK_MASTER_CLIENT = "admin-cli";
     public static final String ENDPOINT_USERS = "/api/v1.3/users/**";
     public static final String ENDPOINT_ORDERS = "/api/v1.3/orders/**";
-    public static final String ENDPOINT_CERTIFICATES = "/api/v1.3/certificates/**";
+    public static final String ENDPOINT_CERTIFICATES = "/api/v1.3/gift_certificates/**";
     public static final String ENDPOINT_TAGS = "/api/v1.3/tags/**";
     public static final String ENDPOINT_AUTHENTICATION = "/api/v1.3/authentication/**";
     public static final String ENDPOINT_SIGNUP = "/api/v1.3/authentication/signup";
